f0349b5b1e550fca0416657eef9f9f50060274cd
Sotnikov Arkadiy <cotnikoarkady@gmail.com>
2021-12-30 22:06:45 +0400

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def hello_world():  # put application's code here
+    user = {"username": "Arkady"}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', user=user, posts=posts)
