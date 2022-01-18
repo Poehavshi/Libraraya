df286b747bb7287fb6551333d4b3e5a48eb94bcd
Sotnikov Arkadiy <cotnikoarkady@gmail.com>
2022-01-18 18:15:00 +0400

    Add edit profile

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ Config file with all preferences of our web application
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     """
     Config class to get config preferences by app.config.from_object(Config)
