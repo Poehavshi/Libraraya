000df5185c7a228f3f51a8412cff4576896015ab
Sotnikov Arkadiy <cotnikoarkady@gmail.com>
2022-01-20 17:54:07 +0400

    Add errors

--- app/models.py
+++ app/models.py
@@ -40,5 +40,8 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
     def __repr__(self):
         return '<Post {}>'.format(self.title)
+
+
